f71c3da95d975bdec3f4e9630a50c3687e2984c6
Sanju0845 <sanjayanand0509@gmail.com>
2025-03-30 11:26:06 +0530

    3rd change

---
+++ react.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyComponent: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <p>This is a simple TSX component.</p>
+        </div>
+    );
+};
+
+export default MyComponent;
